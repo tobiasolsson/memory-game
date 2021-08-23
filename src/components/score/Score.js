0d114b8de97e1d636b94f6cbc5a0dbc85e0446a9
Tobias Olsson <tobias.olsson@pm.me>
2021-08-23 12:09:03 +0200

    Add styling to scoreboard

--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './Score.module.css';
 
 function Score(props) {
   const { score, highScore } = props;
   return (
-    <ul>
+    <ul className={styles.list}>
       <li>Score: {score}</li>
-      <li>High Score: {highScore}</li>
+      <li>
+        High Score: <span className={styles.high}>{highScore}</span>
+      </li>
     </ul>
   );
 }
